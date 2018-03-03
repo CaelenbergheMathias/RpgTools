732dcf149d40b4b8474b1b664ef9f04bc1779e65
Mathias Caelenberghe <mathias.caelenberghe@student.howest.be>
2018-03-03 17:53:17 +0100

    added dice script

---
+++ web/assets/js/dice.js
@@ -0,0 +1,21 @@
+class dice {
+    static rollD4() {
+        return Math.floor((Math.random() * 4) + 1);
+    }
+    static rollD6() {
+        return Math.floor((Math.random() * 6) + 1);
+    }
+    static rollD10() {
+        return Math.floor((Math.random() * 10) + 1);
+    }
+    static rollD12() {
+        return Math.floor((Math.random() * 12) + 1);
+    }
+    static rollD20() {
+        return Math.floor((Math.random() * 20) + 1);
+    }
+    static rollD100() {
+        return Math.floor((Math.random() * 100));
+    }
+}
+//# sourceMappingURL=dice.js.map
