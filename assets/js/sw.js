7a61266e2cdb662ae7c2de5f5de8aeaf8c55b001
Unknown <mathias.caelenberghe@student.howest.be>
2018-03-16 15:32:52 +0100

    added fetch removed caching of script.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -2,8 +2,7 @@ var CACHE_NAME = 'cached_urls';
 var urlsToCache = [
     '../../index.html',
     '../css/reset.css',
-    '../css/screen.css',
-    '../js/script.js'
+    '../css/screen.css'
 ];
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(CACHE_NAME)
@@ -11,4 +10,18 @@ self.addEventListener('install', function (event) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
     }));
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
 });
