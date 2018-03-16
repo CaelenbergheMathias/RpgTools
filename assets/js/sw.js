30fa09ca731c3d434f931919c19cf6bf1884e93c
Unknown <mathias.caelenberghe@student.howest.be>
2018-03-16 15:16:24 +0100

    sw works now?

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,9 +1,9 @@
 var CACHE_NAME = 'cached_urls';
 var urlsToCache = [
-    'index.html',
-    'assets/css/reset.css',
-    'assets/css/screen.css',
-    'assets/js/script.js'
+    '../../index.html',
+    '../css/reset.css',
+    '../css/screen.css',
+    '../js/script.js'
 ];
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(CACHE_NAME)
