83c63849b86a146d07b28ce8f3b2c82841e374f7
Unknown <mathias.caelenberghe@student.howest.be>
2018-05-17 15:00:21 +0200

    activate handle

--- sw.js
+++ sw.js
@@ -54,6 +54,23 @@ self.addEventListener('fetch', function(event) {
     );
 });
 
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['cached_urls'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
 // self.addEventListener('fetch', function(event) {
 //     event.respondWith(
 //         caches.match(event.request)
