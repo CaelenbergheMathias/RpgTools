5cc28d4030ef51edada68dafe5005c00d0346353
Unknown <mathias.caelenberghe@student.howest.be>
2018-05-30 15:32:09 +0200

    change to sw

--- sw.js
+++ sw.js
@@ -1,18 +1,6 @@
 var CACHE_NAME = 'cached_urls';
 var urlsToCache = [
-    'index.html',
-    'assets/css/reset.css',
-    'assets/css/screen.css',
-    'assets/js/jquery-3.1.1.min.js',
-    'assets/js/dice.js',
-    'assets/js/DnD.js',
-    'assets/js/DnDCharacter.js',
-    'assets/js/DnDdata.js',
-    'assets/js/localforage.js',
-    'Pages/DnD.html',
-    'Pages/DnD_character_generator.html',
-    'Pages/CoC.html',
-    'Pages/GURPS.html'
+    '/'
 ];
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(CACHE_NAME)
@@ -71,18 +59,3 @@ self.addEventListener('activate', function(event) {
     );
 });
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(function(response) {
-//                     // Cache hit - return response
-//                     if (response) {
-//                         console.log(response);
-//                         return response;
-//                     }
-//
-//                     return fetch(event.request);
-//                 }
-//             )
-//     );
-// });
